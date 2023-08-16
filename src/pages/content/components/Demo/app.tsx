7b748ce9ee5056bffc5712037c5ce76071226776
Hoang Long <intihad.vuong@gmail.com>
2023-08-16 14:27:22 +0700

    new layout

--- src/pages/content/components/Demo/app.tsx
+++ src/pages/content/components/Demo/app.tsx
@@ -1,9 +1,23 @@
+import { Flex, Title } from "@tremor/react";
+import {  DateRangePicker,DateRangePickerItem, DateRangePickerValue } from "@tremor/react";
+import { useState } from "react";
 
 export default function App() {
+  const [dateRange, setDateRange] = useState<DateRangePickerValue>({
+    from: new Date(2023,1,1),
+    to: new Date()
+  })
   return (
     <main className="px-2 py-2">
-      <p>Hello</p>
-
+      <Flex alignItems="center" justifyContent="between">
+        <Title className="text-3xl text-slate-900">Dashboard</Title>
+        <DateRangePicker
+        className=""
+        value={dateRange}
+        onValueChange={setDateRange}
+        color="slate"
+         />
+      </Flex>
     </main>
   );
 }
